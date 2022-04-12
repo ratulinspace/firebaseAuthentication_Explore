fcb674142717ac47ca58e8dc0e44c4643061edac
ratulinspace <i.shahriarislam.ratul@gmail.com>
2022-04-12 12:57:42 +0600

    signIn & signOut toggle added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import app from './firebase.init';
-import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth';
+import { useState } from 'react';
 
 const auth = getAuth(app);
 
 function App() {
+
+  const [user, setUser] = useState({});
 
   const provider = new GoogleAuthProvider();
 
@@ -12,6 +15,7 @@ function App() {
     signInWithPopup(auth, provider)
       .then(result => {
         const user = result.user;
+        setUser(user);
         console.log(user);
       })
       .catch(error => {
@@ -19,10 +23,28 @@ function App() {
       })
   }
 
+  const handleSignOut = () => {
+    signOut(auth)
+      .then(() => {
+        setUser({})
+      })
+      .catch(() => {
+        setUser({})
+      })
+  }
+
 
   return (
     <div className="App">
-      <button className='btn' onClick={handleGoogleSignIn}>Google Sign In</button>
+      {
+        user.email ? <button className='btn' onClick={handleSignOut}>Sign Out</button>
+          :
+          <button className='btn' onClick={handleGoogleSignIn}>Google Sign In</button>
+      }
+      <br />
+      <h3>User Name: {user.displayName}</h3>
+      <h4>Email: {user.email}</h4>
+      <img src={user.photoURL} alt="" />
     </div>
   );
 }
